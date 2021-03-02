c3ef2d2a7546cec296c14626910c56dabec6902a
Hamza Khan <khanhamza124@gmail.com>
2021-03-02 20:55:20 +0530

    models for the blog, blogger

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Blog, Blogger, Images
 
-# Register your models here.
+# admin.site.register(Blog)
+admin.site.register(Blogger)
+
+#
+
+
+class PostImageAdmin(admin.StackedInline):
+    model = Images
+
+
+@admin.register(Blog)
+class PostAdmin(admin.ModelAdmin):
+    # for enabling  one-to-many relationship for the images
+    inlines = [PostImageAdmin]
+
+    class Meta:
+        model = Blog
+
+
+@admin.register(Images)
+class PostImageAdmin(admin.ModelAdmin):
+    pass
--- api/models.py
+++ api/models.py
@@ -5,17 +5,34 @@ from django.utils import timezone
 # Blog, Bookmark, Video Embed, Image embed, Github Embed, Images(multiple images in the same blog post), Quote.
 
 
-# class Blog(models.Model):
-#     name = models.CharField(max_length=200)
-#     title = models.CharField(max_length=200)
-#     added_by = models.ForeignKey(
-#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     created_date = models.DateTimeField(default=timezone.now)
-#     description = models.CharField(max_length=300)
-#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-#     added_by = models.ForeignKey(
-#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     created_date = models.DateTimeField(default=timezone.now)
+class Blogger(models.Model):
+    name = models.CharField(max_length=200)
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    Bookmark = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
+    image_embed = models.ImageField(upload_to='emb_images', default='')
+    video_embed = models.FileField(upload_to='video/')
+    gist_embed = models.CharField(max_length=200)
+    author = models.ForeignKey(Blogger, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.Bookmark
+
+
+class Images(models.Model):
+    blog = models.ForeignKey(Blog, default=None, on_delete=models.CASCADE)
+    images = models.FileField(upload_to='images/')
+
+    def __str__(self):
+        return self.blog.Bookmark
